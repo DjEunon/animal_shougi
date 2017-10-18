b615b16662c59956c0ba4c4e149a8d4253b642f5
DjEunon <32728493+DjEunon@users.noreply.github.com>
2017-10-18 13:30:54 +0900

    out

--- animal_shougi/animal_shougi.py
+++ animal_shougi/animal_shougi.py
@@ -12,26 +12,141 @@
 #　　　 ＿))ﾆ)
 #　　 ∠∠)＿＞
 #   chocobo-is-food 
-
+import random
 import socket
 import re
 import socketClient
 import checker
+import board_class
 
 
-se=socketClient.soketClient("localhost")
+def get_f(flame):
+    return flame[:1]
 
-BUFSIZE = 1024
-serverName = "localhost"
-#serverName="10.2.72.178"
-serverPort = 4444
+def witch_frame(frame):
+    if frame[-1:]=="1":
+        return "Player1"
+    else:
+        return "Player2"
 
+def lion_next_position(base_position,player):
+    position_list=[]    
+    alf_list=[]
+    num_list=[]
+    base_position_alf=base_position[0:1]
+    base_position_num=base_position[-1:]
+    if base_position_alf=="A"or base_position_alf=="B" or base_position_alf=="C":
+        if base_position_alf=="A":
+            alf_list.append("A")
+            alf_list.append("B")
+        elif base_position_alf=="B":
+            alf_list.append("A")
+            alf_list.append("B")
+            alf_list.append("C")
+        elif base_position_alf=="C":
+            alf_list.append("B")
+            alf_list.append("C")
+        if (int(base_position_num)-1)>0:
+            num_list.append(int(base_position_num)-1)
+        if (int(base_position_num)+1)<5:
+            num_list.append(int(base_position_num)+1)
+        num_list.append(base_position_num)
+        for i in num_list:
+            for j in alf_list:
+                position_list.append(j+str(i))
+    elif base_position_alf=="D" or base_position=="E":
+        position_list=all_position_list()
+    try:
+        position_list.remove(base_position)    
+    except :
+        pass
+    
+    position_list.sort()
+    return position_list
+
+#plan to Loop Unwinding
+#ループ展開したほうが絶対早いことに気づいてしまった。おとなしく書き換えることにする
+ 
+
+def elephant_next_position(base_position,player):
+    position_list=[]    
+    alf_list=[]
+    num_list=[]
+    base_position_alf=base_position[0:1]
+    base_position_num=base_position[-1:]
+    if base_position_alf=="A"or base_position_alf=="B" or base_position_alf=="C":
+        if base_position_alf=="A":
+            alf_list.append("B")
+        elif base_position_alf=="B":
+            alf_list.append("A")
+            alf_list.append("C")
+        elif base_position_alf=="C":
+            alf_list.append("B")
+        if (int(base_position_num)-1)>0:
+            num_list.append(int(base_position_num)-1)
+        if (int(base_position_num)+1)<5:
+            num_list.append(int(base_position_num)+1)
+        for i in alf_list:
+            for j in num_list:
+                 position_list.append(i+str(j))
+        position_list.sort()
+        return position_list
+
+def giraffe_next_position(base_position,player):
+    base_position_alf=base_position[0:1]
+    base_position_num=base_position[-1:]
+    if base_position_alf=="A"or base_position_alf=="B" or base_position_alf=="C":
+        position_list=lion_next_position(base_position,player)
+        for x in elephant_next_position(base_position,player):
+            position_list.remove(x)
+        return position_list
+    
+
+all_position_list=[]
+
+def all_position(args):
+    if len(all_position_list)==0:
+        temp_list=["A","B","C"]
+        for i in temp_list:
+            for j in range(1,5):
+                all_position_list.append(i+str(j))
+    return all_position_lis
+
+se=socketClient.soketClient("10.2.75.151")
+
+board_all_list=[]
 se.game_connect();
 print(se.get_whoami())
 print(se.myturn_now())
 while True:
-    temp=se.send_command(input(""))
-    if temp=="game_end":
-        break
-    else:
-        print(temp)
+    if True:
+        if se.myturn_now:
+           bo=board_class.board_class(se.send_command("board"))
+          
+           board_all_list.append(bo.get_board_dictionary())
+           cha=str(random.choice('ABC'))+str(random.choice('1234'))
+           cho=board_all_list[-1][cha]
+           #print(witch_frame(cho))
+           #print(se.get_whoami())
+           if witch_frame(cho)==se.get_whoami():
+                
+                if get_f(cho)=="l":
+                        a=[]
+                        a=lion_next_position(cha,se.get_whoami())
+                        random.shuffle(a)
+                        se.send_command("mv "+cha+" "+a[0])  
+                elif get_f(cho)=="g":
+                        a=[]
+                        a=giraffe_next_position(cha,se.get_whoami())
+                        random.shuffle(a)
+                        se.send_command("mv "+cha+" "+a[0])
+                elif get_f(cho)=="e":
+                        a=[]
+                        a=elephant_next_position(cha,se.get_whoami())
+                        random.shuffle(a)
+                        se.send_command("mv "+cha+" "+a[0])
+        else:
+            pass
+
+
+
--- animal_shougi/board_class.py
+++ animal_shougi/board_class.py
@@ -7,13 +7,12 @@ class board_class():
     board_dictionary={}
     def __init__(self,board_origin):
         if isinstance(board_origin,str):
-            __str2dic(board_origin)
+            self.str2dic(board_origin)
         elif isinstance(board_origin,dictionary):
             self.board_dictionary=board_origin
-        print(self.board_dictionary)
 
-    def __str2dic(self,string_board):
-        self.board_origin=board_origin
+    def str2dic(self,string_board):
+        self.board_origin=string_board
         temp_list=[]
         temp_list=self.board_origin.split(",")
         for x in temp_list:
@@ -34,92 +33,92 @@ class board_class():
 #plan to Loop Unwinding
 #ループ展開したほうが絶対早いことに気づいてしまった
  
-def lion_next_position(base_position,player):
-    position_list=[]    
-    alf_list=[]
-    num_list=[]
-    base_position_alf=base_position[0:1]
-    base_position_num=base_position[-1:]
-    if base_position_alf=="A"or base_position_alf=="B" or base_position_alf=="C":
-        if base_position_alf=="A":
-            alf_list.append("A")
-            alf_list.append("B")
-        elif base_position_alf=="B":
-            alf_list.append("A")
-            alf_list.append("B")
-            alf_list.append("C")
-        elif base_position_alf=="C":
-            alf_list.append("B")
-            alf_list.append("C")
-        if (int(base_position_num)-1)>0:
-            num_list.append(int(base_position_num)-1)
-        if (int(base_position_num)+1)<5:
-            num_list.append(int(base_position_num)+1)
-        num_list.append(base_position_num)
-        for i in num_list:
-            for j in alf_list:
-                position_list.append(j+str(i))
-    elif base_position_alf=="D" or base_position=="E":
-        position_list=all_position_list()
-    try:
-        position_list.remove(base_position)    
-    except :
-        pass
+#def lion_next_position(base_position,player):
+#    position_list=[]    
+#    alf_list=[]
+#    num_list=[]
+#    base_position_alf=base_position[0:1]
+#    base_position_num=base_position[-1:]
+#    if base_position_alf=="A"or base_position_alf=="B" or base_position_alf=="C":
+#        if base_position_alf=="A":
+#            alf_list.append("A")
+#            alf_list.append("B")
+#        elif base_position_alf=="B":
+#            alf_list.append("A")
+#            alf_list.append("B")
+#            alf_list.append("C")
+#        elif base_position_alf=="C":
+#            alf_list.append("B")
+#            alf_list.append("C")
+#        if (int(base_position_num)-1)>0:
+#            num_list.append(int(base_position_num)-1)
+#        if (int(base_position_num)+1)<5:
+#            num_list.append(int(base_position_num)+1)
+#        num_list.append(base_position_num)
+#        for i in num_list:
+#            for j in alf_list:
+#                position_list.append(j+str(i))
+#    elif base_position_alf=="D" or base_position=="E":
+#        position_list=all_position_list()
+#    try:
+#        position_list.remove(base_position)    
+#    except :
+#        pass
     
-    position_list.sort()
-    return position_list
+#    position_list.sort()
+#    return position_list
 
-#plan to Loop Unwinding
-#ループ展開したほうが絶対早いことに気づいてしまった。おとなしく書き換えることにする
+##plan to Loop Unwinding
+##ループ展開したほうが絶対早いことに気づいてしまった。おとなしく書き換えることにする
  
 
-def elephant_next_position(base_position,player):
-    position_list=[]    
-    alf_list=[]
-    num_list=[]
-    base_position_alf=base_position[0:1]
-    base_position_num=base_position[-1:]
-    if base_position_alf=="A"or base_position_alf=="B" or base_position_alf=="C":
-        if base_position_alf=="A":
-            alf_list.append("B")
-        elif base_position_alf=="B":
-            alf_list.append("A")
-            alf_list.append("C")
-        elif base_position_alf=="C":
-            alf_list.append("B")
-        if (int(base_position_num)-1)>0:
-            num_list.append(int(base_position_num)-1)
-        if (int(base_position_num)+1)<5:
-            num_list.append(int(base_position_num)+1)
-        for i in alf_list:
-            for j in num_list:
-                 position_list.append(i+str(j))
-        position_list.sort()
-        return position_list
+#def elephant_next_position(base_position,player):
+#    position_list=[]    
+#    alf_list=[]
+#    num_list=[]
+#    base_position_alf=base_position[0:1]
+#    base_position_num=base_position[-1:]
+#    if base_position_alf=="A"or base_position_alf=="B" or base_position_alf=="C":
+#        if base_position_alf=="A":
+#            alf_list.append("B")
+#        elif base_position_alf=="B":
+#            alf_list.append("A")
+#            alf_list.append("C")
+#        elif base_position_alf=="C":
+#            alf_list.append("B")
+#        if (int(base_position_num)-1)>0:
+#            num_list.append(int(base_position_num)-1)
+#        if (int(base_position_num)+1)<5:
+#            num_list.append(int(base_position_num)+1)
+#        for i in alf_list:
+#            for j in num_list:
+#                 position_list.append(i+str(j))
+#        position_list.sort()
+#        return position_list
 
-def giraffe_next_position(base_position,player):
-    if base_position_alf=="A"or base_position_alf=="B" or base_position_alf=="C":
-        position_list=lion_next_position(base_position,player)
-        for x in elephant_next_position(base_position,player):
-            position_list.remove(x)
-        return position_list
-     elif base_position_alf=="D" or base_position=="E":
-        position_list=all_position_list()
-    return position_list
+#def giraffe_next_position(base_position,player):
+#    if base_position_alf=="A"or base_position_alf=="B" or base_position_alf=="C":
+#        position_list=lion_next_position(base_position,player)
+#        for x in elephant_next_position(base_position,player):
+#            position_list.remove(x)
+#        return position_list
+#     elif base_position_alf=="D" or base_position=="E":
+#        position_list=all_position_list()
+#    return position_list
 
-all_position_list=[]
+#all_position_list=[]
 
-def all_position(args):
-    if len(all_position_list)==0:
-        temp_list=["A","B","C"]
-        for i in temp_list:
-            for j in range(1,5):
-                all_position_list.append(i+str(j))
-    return all_position_lis
+#def all_position(args):
+#    if len(all_position_list)==0:
+#        temp_list=["A","B","C"]
+#        for i in temp_list:
+#            for j in range(1,5):
+#                all_position_list.append(i+str(j))
+#    return all_position_lis
 
-#board_class("A1 g2, B1 l2, C1 e2, A2 --, B2 c2, C2 --, A3 --, B3 c1, C3 --, A4 e1, B4 l1, C4 g1,")
-def witch_frame(frame):
-    if frame[-1:]==1:
-        return "Player1"
-    else:
-        return "Player2"
+##board_class("A1 g2, B1 l2, C1 e2, A2 --, B2 c2, C2 --, A3 --, B3 c1, C3 --, A4 e1, B4 l1, C4 g1,")
+#def witch_frame(frame):
+#    if frame[-1:]==1:
+#        return "Player1"
+#    else:
+#        return "Player2"
